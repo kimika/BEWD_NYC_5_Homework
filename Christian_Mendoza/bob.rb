b6f2fa7e0816069f5e26b023620e021909ec0c41
Christian Mendoza <christian.mendoza@gmail.com>
2013-11-01 22:17:45 -0400

    Fix formatting

--- Christian_Mendoza/bob.rb
+++ Christian_Mendoza/bob.rb
@@ -1,37 +1,39 @@
 class Bob
-	def hey (message)
-		response (message)
-	end
+  def hey (message)
+    respond (message)
+  end
 
-	def response (msg)
-		if is_silent? (msg)
-			"Fine. Be that way!"
-		elsif is_yelling? (msg)
-			"Woah, chill out!"
-		elsif is_questioning? (msg)
-			"Sure."
-		else
-			"Whatever."
-		end
-	end
+  def respond (msg)
+    response = ["Fine. Be that way!", "Woah, chill out!", "Sure.", "Whatever."]
+    
+    if is_silent? (msg)
+      response[0]
+    elsif is_yelling? (msg)
+      response[1]
+    elsif is_questioning? (msg)
+      response[2]
+    else
+      response[3]
+    end
+  end
 
-	def is_silent? (msg)
-		msg.squeeze.length<2
-	end
+  def is_silent? (msg)
+    msg.squeeze.length<2
+  end
 
-	def is_yelling? (msg)
-		msg == msg.upcase
-	end
+  def is_yelling? (msg)
+    msg == msg.upcase
+  end
 
-	def is_questioning? (msg)
-		msg.end_with? "?"
-	end
+  def is_questioning? (msg)
+    msg.end_with? "?"
+  end
 
-	def has_numbers? (msg)
-		if msg.to_i != 0
-			true
-		else
-			false
-		end
-	end
+  def has_numbers? (msg)
+    if msg.to_i != 0
+      true
+    else
+      false
+    end
+  end
 end
