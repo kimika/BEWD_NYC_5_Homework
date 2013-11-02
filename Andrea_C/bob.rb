64805244b5035d85fcb8c0ccef533da7e64b1d7a
Andrea <cremese@gmail.com>
2013-11-02 17:53:25 -0400

    second go, leaner meaner faster (??)

--- Andrea_C/bob.rb
+++ Andrea_C/bob.rb
@@ -1,4 +1,5 @@
-# Not very elegant but functioning version
+# Version 03 - responds to all tests - impoved as no duplicates of outputs required.
+# Only one more condition required
 
 class Bob
   def hey(message)
@@ -6,33 +7,74 @@ class Bob
     if message =="" || message.include?("  ")
       "Fine. Be that way!"
     else
-    
-    if message.include?("!") && message.include?("?")
-      "Sure."
+      if !/[a-z]/.match(message) && /[A-Z]/.match(message) 
+      "Woah, chill out!"
     else
-    
-   if message[-1,1].eql?("!")
-      if /\d/.match(message[1..-1]) || /\W/.match(message[1..-1]) 
-        if message.include?("'")
-          "Whatever."
-        else
-        "Woah, chill out!"
-      end
+      if message[-1,1].eql?("?")
+        "Sure."
       else
         "Whatever."
       end
-    elsif message[-1,1].eql?("?")
-      if message[-2,1].downcase!
-        "Woah, chill out!"
-      else
-        "Sure."
-      end
-    elsif /[A-Z][A-Z][A-Z][A-Z]/.match(message[1..-1])
-      "Woah, chill out!"
-    else
-    "Whatever."
+    end
   end
 end
 end
-end
-end
+
+# Version 02 - responds to all tests beside bonuses - fails bonuses / impoved as no duplicates of outputs required.
+# class Bob
+#   def hey(message)
+#     message = message.tr("\n","")
+#     if message =="" || message.include?("  ")
+#       "Fine. Be that way!"
+#     else
+#       if !/[a-z]/.match(message)
+#       "Woah, chill out!"
+#     else
+#       if message[-1,1].eql?("?")
+#         "Sure."
+#       else
+#         "Whatever."
+#       end
+#     end
+#   end
+# end
+# end
+
+
+# Version 01 Not very elegant but functioning version - I will try to make it look prettier and maybe more readable.
+# class Bob
+#   def hey(message)
+#     message = message.tr("\n","")
+#     if message =="" || message.include?("  ")
+#       "Fine. Be that way!"
+#     else
+    
+#     if message.include?("!") && message.include?("?")
+#       "Sure."
+#     else
+    
+#    if message[-1,1].eql?("!")
+#       if /\d/.match(message[1..-1]) || /\W/.match(message[1..-1]) 
+#         if message.include?("'")
+#           "Whatever."
+#         else
+#         "Woah, chill out!"
+#       end
+#       else
+#         "Whatever."
+#       end
+#     elsif message[-1,1].eql?("?")
+#       if message[-2,1].downcase!
+#         "Woah, chill out!"
+#       else
+#         "Sure."
+#       end
+#     elsif /[A-Z][A-Z][A-Z][A-Z]/.match(message[1..-1])
+#       "Woah, chill out!"
+#     else
+#     "Whatever."
+#   end
+# end
+# end
+# end
+# end
