24bdc8814a4f029216406b5be09a8ab6cf10142a
Danielle McDonnough <daniellermcd@gmail.com>
2013-11-04 15:51:45 -0500

    bob bonus tests now passing

--- Danielle_McDonnough/bob.rb
+++ Danielle_McDonnough/bob.rb
@@ -1,11 +1,30 @@
+# solution to bob homework
+
+# class Bob
+#   def hey message
+#     if message.strip.empty?
+#       'Fine. Be that way!'
+#     elsif message == message.upcase
+#       'Woah, chill out!'
+#     elsif message.end_with? '?'
+#       'Sure.'
+#     else
+#       'Whatever.'
+#     end
+#   end
+# end
+
+# bonus tests now pass!
+
 class Bob
   def hey message
+    message_contains_only_numbers = message.match(/[a-zA-Z]/).nil?
     if message.strip.empty?
       'Fine. Be that way!'
-    elsif message == message.upcase
-      'Woah, chill out!'
-    elsif message.end_with? '?'
+    elsif (message.end_with? '?') && (message != message.upcase || message_contains_only_numbers)
       'Sure.'
+    elsif !message_contains_only_numbers && message == message.upcase
+      'Woah, chill out!'
     else
       'Whatever.'
     end
