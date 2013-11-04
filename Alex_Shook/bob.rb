de8e38bfcec3f0bb55c3d560813f35881ee156db
Alex Shook <alexandra.shook@gmail.com>
2013-11-03 19:49:25 -0500

    had time to improve but still working on bonus

--- Alex_Shook/bob.rb
+++ Alex_Shook/bob.rb
@@ -1,13 +1,26 @@
 class Bob
-  def hey(phrase)
-    if phrase.empty? || phrase.squeeze.length < 2
-      'Fine. Be that way!'
-    elsif phrase == phrase.upcase
-      'Woah, chill out!'
-    elsif phrase.end_with? '?'
-      'Sure.'
-    else
-      'Whatever.'
-    end
+  def hey(message)
+    response = ['Fine. Be that way!', 'Woah, chill out!', 'Sure.', 'Whatever.']
+      if is_silent?(message)
+        response[0]    
+      elsif is_yelling?(message)
+        response[1]
+      elsif is_question?(message)
+        response[2]  
+      else
+        response[3]
+      end
+  end
+
+  def is_question?(message)
+    message.end_with? '?'
+  end
+
+  def is_yelling?(message)
+    message == message.upcase
+  end
+
+  def is_silent?(message)
+    message.empty? || message.squeeze.length < 2
   end
 end
